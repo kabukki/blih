4b8a73e3992c90331e103fbf438287d00054cf21
Lucien Le Roux <lucien.le-roux@epitech.eu>
2017-12-09 20:55:48 +0100

    Fix filter in getACL endpoint

--- endpoints.js
+++ endpoints.js
@@ -60,7 +60,7 @@ module.exports = [
         path: r => `/repository/${r}/acls`,
         method: 'GET',
         transform: data => Object.keys(data)
-            .filter(c => c.rights.length).sort()
+            .filter(c => c.length && data[c].length).sort()
             .map(c => ({
                 name: c,
                 rights: data[c]
