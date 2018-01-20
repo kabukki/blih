3b1a649122d17a85383a328c604be56617b5b6b2
Lucien Le Roux <lucien.le-roux@epitech.eu>
2018-01-20 16:14:30 +0100

    Added unit tests

---
+++ test/constructor.spec.js
@@ -0,0 +1,82 @@
+/* eslint-env mocha */
+
+const assert = require('assert');
+const Blih = require('..');
+const endpoints = require('../src/endpoints');
+const crypto = require('crypto');
+
+const [email, password, token] = ['email', 'password', 'token'];
+
+describe('Constructor', function () {
+
+    describe('parameters', function () {
+
+        it('should throw with no parameter', function () {
+            assert.throws(_ => {
+                new Blih();
+            }, /Missing credentials/);
+        });
+
+        it('should throw when email not provided', function () {
+            assert.throws(_ => {
+                new Blih({});
+                new Blih({ email: undefined });
+                new Blih({ email: '' });
+            }, /Email is mandatory to authenticate/);
+        });
+
+        it('should throw when password/token not provided', function () {
+            assert.throws(_ => {
+                new Blih({ email });
+                new Blih({ email, password: undefined });
+                new Blih({ email, password: '' });
+                new Blih({ email, token: undefined });
+                new Blih({ email, token: '' });
+            }, /A password or token is needed to authenticate/);
+        });
+
+        it('should not throw when credentials provided', function () {
+            assert.doesNotThrow(_ => {
+                new Blih({ email, password });
+                new Blih({ email, token });
+                new Blih({ email, password, token });
+            });
+        });
+
+    });
+
+    describe('token', function () {
+
+        it('should match', function () {
+            const expected = crypto.createHash('sha512').update(password).digest('hex');
+            const actual = new Blih({ email, password }).token;
+            assert.equal(actual, expected, 'Tokens differ');
+        });
+
+        it('should not match', function () {
+            const expected = crypto.createHash('sha512').update('not match').digest('hex');
+            const actual = new Blih({ email, password }).token;
+            assert.notEqual(actual, expected, 'Tokens match');
+        });
+
+    });
+
+    describe('endpoints', function () {
+
+        it('should exist', function () {
+            const expected = endpoints.map(e => e.name);
+            const api = new Blih({ email, password });
+            for (const endpoint of expected) {
+                assert.ok(api[endpoint], 'Endpoint does not exist');
+                assert.equal(typeof api[endpoint], 'function', 'Endpoint is not a function');
+            }
+        });
+
+        it('should include static ping', function () {
+            assert.ok(Blih['ping'], 'Static ping endpoint does not exist');
+            assert.equal(typeof Blih['ping'], 'function', 'Static ping endpoint is not a function');
+        });
+
+    });
+
+});
